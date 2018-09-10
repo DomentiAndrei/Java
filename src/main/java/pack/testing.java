a7eab4c228ed55e7a1eb5599efabd6c2c1c4ffb9
Andrei <Andrei@192.168.0.19>
2018-09-10 20:11:46 +0300

    first file

---
+++ src/main/java/pack/testing.java
@@ -0,0 +1,19 @@
+package pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class testing {
+	@Test
+	public void test() {
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://mail.ru");
+		
+		
+		
+		
+	}
+}
